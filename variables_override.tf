5d35def836f19246e02a54d96d6aa6eabf512e8f
Scott Brightwell <scott@brightwell.org>
2018-08-14 23:16:34 -0400

    changed network subnets to match customer(0) conventions

---
+++ variables_override.tf
@@ -0,0 +1,29 @@
+variable "management_cidr" {
+  type        = "string"
+  description = "cidr for common infrastructure management subnet"
+  default     = "10.0.0.0/26"
+}
+
+variable "pas_cidr" {
+  type        = "string"
+  description = "cidr for pas deployment subnet"
+  default     = "192.168.4.0/24"
+}
+
+variable "services_cidr" {
+  type        = "string"
+  description = "cidr for pas services subnet"
+  default     = "192.168.8.0/24"
+}
+
+variable "pks_cidr" {
+  type        = "string"
+  description = "cidr for pks cluster subnet"
+  default     = "172.20.4.0/24"
+}
+
+variable "pks_services_cidr" {
+  type        = "string"
+  description = "cidr for pks services subnet"
+  default     = "172.20.8.0/24"
+}
